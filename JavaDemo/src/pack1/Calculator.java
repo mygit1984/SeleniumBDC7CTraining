027ec82a4c367dc12f0a2ae8bf10b47bd6e5b73c
kannan.ramalingam <kannan.ramalingam@BDC4-L-5LW6QQ2.dir.svc.accenture.com>
2019-08-13 17:05:22 +0530

    commit1

---
+++ JavaDemo/src/pack1/Calculator.java
@@ -0,0 +1,13 @@
+package pack1;
+
+public class Calculator {
+	int empNo;	//class variable or instance variable
+	public static void main(String[] args) {
+		int mark1=10;//local variable
+		int mark2=20;
+		int totalMark=mark1+mark2;
+		System.out.println("The total mark:"+totalMark);
+		
+	}
+
+}
